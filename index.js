4f2d29c8b6e220b5df02b1f5067f5b58c2511868
rachel2647 <r0548572647@gmail.com>
2023-05-15 23:28:56 +0300

    last

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import express from 'express'
 import { createRequire } from 'module'
 
-const require = createRequire(import.meta.url)
+const require=createRequire(import.meta.url)
 const app = express();
 
 const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
 
-app.get('/', (req, res) => {
-    sdk.auth('rnd_Dj9BLHfuoLRdg6ECeaCYN55USn2F');
-    sdk.getServices({ limit: '20' })
-        .then(({ data }) => console.log(data))
-        .catch(err => console.error(err))
+app.get('/',(req,res)=>{
+sdk.auth('rnd_Dj9BLHfuoLRdg6ECeaCYN55USn2F');
+sdk.getServices({limit: '20'})
+  .then(({ data }) => res.send(data))
+  .catch(err => console.error(err))
 });
-
 
 
 app.listen(3000, () => {
