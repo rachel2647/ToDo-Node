d52eb208146b00a38b8694ba22880b3edaac62de
rachel2647 <r0548572647@gmail.com>
2023-05-15 22:31:51 +0300

    first-commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+const app = express();
+
+const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
+
+app.get('/', (req, res) => {
+    sdk.auth('rnd_Dj9BLHfuoLRdg6ECeaCYN55USn2F');
+    sdk.getServices({ limit: '20' })
+        .then(({ data }) => console.log(data))
+        .catch(err => console.error(err))
+});
+
+
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000');
+});
